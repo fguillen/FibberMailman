2d1071a9f04b671b97980c4c170edd5926879d24
Fernando Guillen <fguillen.mail@gmail.com>
2010-08-03 10:41:07 +0200

    rdoc

--- lib/fibber_mailman.rb
+++ lib/fibber_mailman.rb
@@ -14,6 +14,21 @@ class FibberPop
   end
 end
 
+#
+# Example of use:
+#     raw_mails = [
+#       File.read( "/fixtures/mail1.raw_mail" ) ),
+#       File.read( "/fixtures/mail2.raw_mail" ) ),
+#     ]
+#     
+#     FibberMailman.lie_to_me( raw_mails ) do
+#       <your code that uses the Net::POP3.start on any point>
+#     end
+#     
+# Not any real POP call will be done.
+# 
+# The mock will simulate that the mails received was the fake **raw_mails**.
+#
 class FibberMailman
   def self.lie_to_me( raw_mails )
     @@raw_mails = raw_mails
